2012720996b78bebfb920ce3c98b51c7d7eabc03
cromasc <joao_marcos_calixto@hotmail.com>
2024-03-16 11:32:50 -0300

    first commit

---
+++ web-page/javascript.js
@@ -0,0 +1,8 @@
+const label = document.querySelector("label");
+const button = document.querySelector("button");
+const input = document.querySelector('#imageInput');
+const fileName = document.querySelector('#file-name');
+
+input.addEventListener('change', function() {
+    
+});
